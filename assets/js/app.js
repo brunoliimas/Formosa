5a11992d067019c50e947e4c66e41ee9893a1b43
Bruno Lima <ibrunoliimas@gmail.com>
2018-08-16 14:43:28 -0300

    Fazer site em PHP

---
+++ assets/js/app.js
@@ -0,0 +1,165 @@
+
+  // creating Angular Module====Contact-Us Form
+  var websiteApp = angular.module('websiteApp', []);
+  websiteApp.controller('FormController',function($scope, $http,$timeout) {
+  	$scope.successStatus = true;
+  	$scope.formData = {};
+	  $scope.checkOption = function(){
+	  	$scope.selectOption = false;
+	  }
+	  $scope.submitcotactusForm = function(data) {
+	  	$scope.successStatus = true;
+	  	if(data == true){
+	  		//$scope.message = "Please fill the option";
+	  		if(!$scope.formData.selectOption)
+	  		$scope.selectOption = true;
+	  		return false;
+	  	}
+	  	else if(!$scope.formData.selectOption){
+	  		
+	  		$scope.selectOption = true;
+	  		//$scope.message = "Please select option";
+	  		return false;
+	  	}
+	  	else{
+	  		$scope.message = ""
+		$http({
+		method : 'POST',
+		url : 'process.php',
+		data : $.param({data :$scope.formData,pageId:'contactUs'}), // pass in data as strings
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+	  })
+		.success(function(data) {
+			$scope.successStatus = false;
+			 $scope.formData = {};
+			/*
+			 $timeout(function(){
+						 $("#contact input,#contact select,#contact textarea").removeClass("error");
+						 },10);*/
+			
+			 $('#success').fadeIn(1000);
+			 $timeout(function(){
+			 $('#success').fadeOut(1000);
+			 },1000);
+		 });
+	
+		}
+	   };
+	
+	
+	/************ footer form ********/
+	
+	 $scope.footerformData = {};
+	 $scope.successStatus = true;	
+	  $scope.submitfooterForm = function(data) {
+	  	$scope.successStatus = true;
+	  	var status = true;
+	  	if(data){
+	  		
+	  	return false;	  	
+	  	}
+	  	else{	  		
+	  		
+		$http({
+		method : 'POST',
+		url : 'process.php',
+		data : $.param({data :$scope.footerformData,pageId:'contactUs'}), // pass in data as strings
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+	  })
+		.success(function(data) {
+			$scope.successStatus = false;
+			 $scope.footerformData = {};
+			
+			 $('#footerSuccess').fadeIn(1000);
+			 $timeout(function(){
+			 	$('#footerSuccess').fadeOut(1000);
+			 },1000);
+		 });
+		};
+	   
+	   };
+	   
+	    /************ Testinomial file one *************/
+	   $scope.formDatatest = {};
+	   $scope.submitcotactusFormtest = function(data) {
+	  	$scope.successStatus = true;
+	  	if(data){
+	  		return false;
+	  	}
+	  	else{
+	  		$scope.message = ""
+		$http({
+		method : 'POST',
+		url : 'process.php',
+		data :$.param({data :$scope.formDatatest,pageId:'testimonial'}), // pass in data as strings
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+	  })
+		.success(function(data) {
+			$scope.successStatus = false;
+			 $scope.formDatatest = {};
+		 });
+		}
+	   };
+	   
+	  
+	
+});
+websiteApp.controller('homepageTwoCltr',function($scope,$http){
+	$scope.successStatus = true;
+	$scope.homeformData = {};	 
+	  $scope.submitForm = function(data) {
+	  	$scope.successStatus = true;
+	  	var status = true;
+	  	if(data){
+	  		
+	  	return false;	  	
+	  	}
+	  	else{	  		
+	  	
+		$http({
+		method : 'POST',
+		url : 'process.php',
+		data : $.param($scope.homeformData), // pass in data as strings
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+	  })
+		.success(function(data) {
+			$scope.successStatus = false;
+			$scope.homeformData = {};
+			
+		 
+		 });
+		};
+	   
+	   };
+	   
+	    $scope.footerformData = {};
+	 $scope.successStatus = true;	
+	  $scope.submitfooterForm = function(data) {
+	  	$scope.successStatus = true;
+	  	var status = true;
+	  	if(data){
+	  		
+	  	return false;	  	
+	  	}
+	  	else{	  		
+	  		
+		$http({
+		method : 'POST',
+		url : 'process.php',
+		data : $.param($scope.footerformData), // pass in data as strings
+		headers : { 'Content-Type': 'application/x-www-form-urlencoded' } // set the headers so angular passing info as form data (not request payload)
+	  })
+		.success(function(data) {
+			$scope.successStatus = false;
+			 $scope.footerformData = {};
+			
+			 $('#footerSuccess').fadeIn(1000);
+			 $timeout(function(){
+			 	$('#footerSuccess').fadeOut(1000);
+			 },1000);
+		 });
+		};
+	   
+	   };
+	  
+})
---
+++ content.php
@@ -0,0 +1,220 @@
+<!--Content Area Start-->
+<div id="content">
+    <!-- service-wrap section start here -->
+    <section class="service-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 service-box">
+                    <div class="main-services text-right">
+                        <div class="heading-wrap">
+                            <h2> Formosa Garden </h2>
+                            <span> A Empresa </span>
+                        </div>
+                        <p>
+                            Como filosofia de promover qualidade de vida através do contato com o meio ambiente, a estrutura do Formosa Garden permite aos clientes viverem uma experiência única em um espaço com mais de três mil espécies de flores, plantas e árvores frutíferas das mais tradicionais às exóticas.
+                        </p>
+                        <a href="#" class="btn more-btn"> Conheça a Empresa</a>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6 service-listing">
+                            <img src="assets/images/service-img-1.jpg" alt="service"/>
+                            <div class="service-inner-box">
+                                <strong>Flores</strong>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 service-listing">
+                            <img src="assets/images/service-img-2.jpg" alt="service"/>
+                            <div class="service-inner-box">
+                                <strong>Vasos</strong>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 service-listing margin_b">
+                            <img src="assets/images/service-img-4.jpg" alt="service"/>
+                            <div class="service-inner-box analysis">
+                                <strong>Passaros</strong>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6 service-listing margin_b">
+                            <img src="assets/images/service-img-3.jpg" alt="service"/>
+                            <div class="service-inner-box">
+                                <strong>Adubo e suplementação</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 triangle-img">
+                    <img src="assets/images/sky-blue-triangle.png" alt="sky-triangel" class="blue-triangle"/>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- projects-wrap section start here -->
+    <section class="projects-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 pull-right">
+                    <div class="main-services main-project">
+                        <div class="heading-wrap">
+                            <h2> Paisagismo </h2>
+                            <span>Saiba mais </span>
+                        </div>
+                        <p>
+                            Aqui na Formosa Garden além do frescor e beleza das flores, você encontra um setor especializado para deixar a sua casa bonita todos os dias.
+                        </p>
+                        <a href="#" class="btn more-btn"> Acessar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 main-project-wrap">
+                    <div class="project-list list-one">
+                        <figure>
+                            <img src="assets/images/project-img-1.png" alt="projects"/>
+                            <a href="#" class="more-link-wrapper"><span class="more-info-link"><span>+</span></span></a>
+                        </figure>
+                    </div>
+                    <div class="project-list list-two">
+                        <figure>
+                            <img src="assets/images/project-img-2.png" alt="projects"/>
+                            <a href="#" class="more-link-wrapper"><span class="more-info-link"><span>+</span></span></a>
+                        </figure>
+                    </div>
+                    <div class="project-list list-two list-three">
+                        <figure>
+                            <img src="assets/images/project-img-3.png" alt="projects"/>
+                            <a href="#" class="more-link-wrapper"><span class="more-info-link"><span>+</span></span></a>
+                        </figure>
+                    </div>
+                    <div class="project-list list-two list-four">
+                        <figure>
+                            <img src="assets/images/project-img-4.png" alt="projects"/>
+                            <a href="#" class="more-link-wrapper"><span class="more-info-link"><span>+</span></span></a>
+                        </figure>
+                    </div>
+                    <div class="project-list list-two list-five">
+                        <figure>
+                            <img src="assets/images/project-img-5.png" alt="projects"/>
+                            <a href="#" class="more-link-wrapper"><span class="more-info-link"><span>+</span></span></a>
+                        </figure>
+                    </div>
+                    <div class="project-list list-two list-six">
+                        <figure>
+                            <img src="assets/images/project-img-6.png" alt="projects"/>
+                            <a href="#" class="more-link-wrapper"><span class="more-info-link"><span>+</span></span></a>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="news-wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 heading-wrap">
+                    <h2> Arranjos florais </h2>
+                    <span>Serviços</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 blog-news-wrap">
+                    <div class="blog-comment-info"></div>
+                    <div class="blog-news">
+                        <figure>
+                            <img src="assets/images/news-pic-1.jpg" alt="news-blog" />
+                        </figure>
+                        <div class="blog-caption">
+                            <h4>
+                                <a href="news.html"> 
+                                Arranjos
+                                </a>
+                            </h4>
+                            <p>
+                                O Shopping Garden quer ajudar a você a presentear pessoas queridas e a deixar a sua casa sempre bonita.
+                            </p>
+                            <a href="#" class="btn more-btn"> Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 blog-news-wrap">
+                    <div class="blog-comment-info"></div>
+                    <div class="blog-news">
+                        <figure>
+                            <img src="assets/images/news-pic-2.jpg" alt="news-blog" />
+                        </figure>
+                        <div class="blog-caption">
+                            <h4>
+                                <a href="news.html"> 
+                                Montagem de vasos
+                                </a>
+                            </h4>
+                            <p>
+                                No Shopping Garden existe uma equipe especializada para montar e plantar adequadamente o seu vaso
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="team-wrap">
+        <div class="container">
+            <div class="heading-wrap text-center"></div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-3 member-wrap">
+                    <figure>
+                        <img src="assets/images/team-member-1.jpg" alt="team-member" />
+                    </figure>
+                    <div class="member-contact-info text-center">
+                        <span class="name-label"> <strong>Flores</strong> <span>Diversas Especies</span> </span>
+                        <div class="media-wrap"></div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3 member-wrap">
+                    <figure>
+                        <img src="assets/images/team-member-2.jpg" alt="team-member" />
+                    </figure>
+                    <div class="member-contact-info text-center">
+                        <span class="name-label"> <strong>Arranjos</strong> <span>Lindos arranjos especiais</span> </span>
+                        <div class="media-wrap"></div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3 member-wrap">
+                    <figure>
+                        <img src="assets/images/team-member-3.jpg" alt="team-member" />
+                    </figure>
+                    <div class="member-contact-info text-center">
+                        <span class="name-label"> <strong>Passaros</strong> <span>Tenha uma ave como pet</span> </span>
+                        <div class="media-wrap"></div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-3 member-wrap">
+                    <figure>
+                        <img src="assets/images/team-member-4.jpg" alt="team-member" />
+                    </figure>
+                    <div class="member-contact-info text-center">
+                        <span class="name-label"> <strong>Encomendas</strong> <span>Presentes exclusivos</span> </span>
+                        <div class="media-wrap"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="business-solution">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-lg-6 heading-wrap">
+                    <h3>Solicite um orçamento</h3>
+                    <span>Entre em contato através de nosso formulario online ou ligue 2115-9595 </span>
+                </div>
+                <div class="col-xs-12 col-sm-3 col-lg-2 pull-right">
+                    <a href="#" class="btn more-btn pull-right">Contato</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<!--Content Area End-->
